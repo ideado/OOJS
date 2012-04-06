b7f24082ede5349dc3e0912b9dd691c2dd69015a
miss bubbles <ioana.petrea@gmail.com>
2012-04-06 21:56:40 +0300

    c

--- inheritance_1.js
+++ inheritance_1.js
@@ -140,7 +140,44 @@ my.__proto__.__proto__.__proto__.constructor;
 my.__proto__.constructor;
 my.__proto__.__proto__.constructor;
 my.__proto__.__proto__.__proto__.constructor;
-
 /*--------------------------------------------
 Uber—Access to the Parent from a Child Object
---------------------------------------------*/ 
+--------------------------------------------*/
+function Shape(){}
+// augment prototype
+Shape.prototype.name = 'shape';
+Shape.prototype.toString = function(){
+var result = [];
+if (this.constructor.uber) {
+result[result.length] = this.constructor.uber.toString();
+}
+result[result.length] = this.name;
+return result.join(', ');
+};
+function TwoDShape(){}
+// take care of inheritance
+var F = function(){};
+F.prototype = Shape.prototype;
+TwoDShape.prototype = new F();
+TwoDShape.prototype.constructor = TwoDShape;
+TwoDShape.uber = Shape.prototype;
+// augment prototype
+TwoDShape.prototype.name = '2D shape';
+function Triangle(side, height) {
+this.side = side;
+this.height = height;
+}
+// take care of inheritance
+var F = function(){};
+F.prototype = TwoDShape.prototype;
+Triangle.prototype = new F();
+Triangle.prototype.constructor = Triangle;
+Triangle.uber = TwoDShape.prototype;
+// augment prototype
+Triangle.prototype.name = 'Triangle';
+Triangle.prototype.getArea = function(){return this.side * this.height / 2;}
+var my = new Triangle(5, 10);
+my.toString();
+/*------------------------------------------------
+Isolating the Inheritance Part into a Function
+--------------------------------------------------*/
