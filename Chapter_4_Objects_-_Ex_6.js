c13c271fbabe2c90d5b4297b9130c94bc6eb9706
miss bubbles <ioana.petrea@gmail.com>
2012-04-06 16:07:45 +0300

    commit message

--- Chapter_4_Objects_-_Ex_6.js
+++ Chapter_4_Objects_-_Ex_6.js
@@ -6,19 +6,27 @@ Imagine Array() doesn't exist and the array literal notation doesn't exist eithe
 function MyArray()
 {
     this.a=[];
+    var r=[];
     if(arguments.length>0)
     {
         for(var i=0;i<arguments.length;i++)
         {
-            this.a[i]=arguments[i];
+            r[i]=arguments[i];
         }
     }
-    this.length=a.length;
+    this.a=r;
+    this.length=arguments.length;
 };
 MyArray.prototype.push=function(n)
 {
-    this.a[length++]=n;
+    
+    this.a[a.length]=n;
+    this.length++;    
     return this.a;
+}
+MyArray.prototype.index=function(n)
+{
+    return this.a[n];
 }
 MyArray.prototype.toString=function()
 {
@@ -33,7 +41,26 @@ MyArray.prototype.toString=function()
     }
     return r;
 }
+MyArray.prototype.join=function(s)
+{
+    var r="";
+    if(this.length>0)
+    {
+        r+=this.a[0];
+        for(var i=1;i<this.length;i++)
+        {
+            r+=s+this.a[i];
+        }
+    }
+    return r;
+}
 var a=new MyArray(1,2,3,"test");
 a.toString();
 a.length;
-a[a.length-1];
+a.push("boo");
+a.push("boo");
+a.push("boo");
+a.push("boo");
+a.toString();
+a.join(';');
+a.join(' isn\'t ');
